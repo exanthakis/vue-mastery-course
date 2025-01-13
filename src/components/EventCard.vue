1c5342412a008c117a952b6f12ab9636f55aec1e
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-01-13 11:45:23 +0200

    Create Event card sfc

---
+++ src/components/EventCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="event-card">
+    <h2>{{ title }}</h2>
+    <span>@{{ time }} on {{ date }}</span>
+  </div>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
