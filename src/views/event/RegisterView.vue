da88734e4e2dde7b28ace49c6ab717fded1a1947
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-01-14 11:23:11 +0200

    Added programmatic navigation to register page

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const props = defineProps(['event'])
+
+const router = useRouter()
+
+const register = () => {
+  router.push({ name: 'event-details', id: props.event.id })
+}
+</script>
+
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me!</button>
 </template>
