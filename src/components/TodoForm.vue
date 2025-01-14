3694994f701b3fc69be854d83b219739949aab72
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-01-14 14:46:19 +0200

    Mutating state with actions

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,5 +1,27 @@
-<script setup></script>
+<script setup>
+import { useTodoListStore } from '@/stores/todoList'
+import { ref } from 'vue'
+// import {useTodoListStore} from '@store'
 
-<template><div>todoform</div></template>
+const todo = ref('')
+const store = useTodoListStore()
+
+const addItemAndClear = (item) => {
+  if (item.length === 0) {
+    return
+  }
+  store.addTodo(item)
+  todo.value = ''
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="addItemAndClear(todo)">
+      <input v-model="todo" type="text" />
+      <button>Add</button>
+    </form>
+  </div>
+</template>
 
 <style scoped></style>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -4,6 +4,20 @@ export const useTodoListStore = defineStore('todoList', {
   state: () => {
     return {
       todoList: [],
+      id: 0,
     }
+  },
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false })
+    },
+    deleteTodo(id) {
+      this.todoList = this.todoList.filter((todo) => todo.id !== id)
+    },
+  },
+  getters: {
+    list() {
+      return this.todoList
+    },
   },
 })
