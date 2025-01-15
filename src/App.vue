c654d4d3e3ae095bb95f02ac0b9c3c862eec2508
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-01-15 11:24:51 +0200

    Revert "Create initial pinia store"
    This reverts commit da5a36b0539d26c00ecf079c20c5046ba14d7d6d.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,62 @@
 <script setup>
-import TodoApp from './components/TodoApp.vue'
+import { inject } from 'vue'
+
+const GStore = inject('GStore')
 </script>
 
 <template>
-  <TodoApp />
+  <div id="layout">
+    <div id="flashMessage" v-if="GStore.flashMessage">
+      {{ GStore.flashMessage }}
+    </div>
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink :to="{ name: 'event-list' }">Events</RouterLink> |
+          <RouterLink :to="{ name: 'about' }">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <RouterView />
+  </div>
 </template>
 
-<style></style>
+<style>
+#layout {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav,
+#nav {
+  padding: 30px;
+}
+nav a,
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active,
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+h2 {
+  font-size: 20px;
+}
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
+}
+</style>
