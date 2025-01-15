95b6104b966e61c18ee717d6a954ca852aa3746b
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-01-15 11:24:18 +0200

    Revert "Mutating state with actions"
    This reverts commit 3694994f701b3fc69be854d83b219739949aab72.

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,4 @@ import TodoApp from './components/TodoApp.vue'
   <TodoApp />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+<style></style>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -4,20 +4,6 @@ export const useTodoListStore = defineStore('todoList', {
   state: () => {
     return {
       todoList: [],
-      id: 0,
     }
-  },
-  actions: {
-    addTodo(item) {
-      this.todoList.push({ item, id: this.id++, completed: false })
-    },
-    deleteTodo(id) {
-      this.todoList = this.todoList.filter((todo) => todo.id !== id)
-    },
-  },
-  getters: {
-    list() {
-      return this.todoList
-    },
   },
 })
