5dc5fb8b822b04f76a1f88782f774944c4c192dd
Xanthakis Emmanouil <manolis.ksanthakis@gmail.com>
2025-01-22 11:32:10 +0200

    Revert "Mutating state with actions"
    This reverts commit 3694994f701b3fc69be854d83b219739949aab72.

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,27 +1,5 @@
-<script setup>
-import { useTodoListStore } from '@/stores/todoList'
-import { ref } from 'vue'
-// import {useTodoListStore} from '@store'
+<script setup></script>
 
-const todo = ref('')
-const store = useTodoListStore()
-
-const addItemAndClear = (item) => {
-  if (item.length === 0) {
-    return
-  }
-  store.addTodo(item)
-  todo.value = ''
-}
-</script>
-
-<template>
-  <div>
-    <form @submit.prevent="addItemAndClear(todo)">
-      <input v-model="todo" type="text" />
-      <button>Add</button>
-    </form>
-  </div>
-</template>
+<template><div>todoform</div></template>
 
 <style scoped></style>
